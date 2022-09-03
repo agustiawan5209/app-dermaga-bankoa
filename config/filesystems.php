a2f306e822aabd8c6bdfc9a33947ddefc359970b
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-09-03 18:25:58 +0800

    Update filesystems.php
    Buat Folder Bukti

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,13 @@ return [
             'throw' => false,
         ],
 
+        'upload' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/upload',
+            'visibility' => 'upload',
+            'throw' => false,
+        ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
@@ -71,6 +78,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('bukti') => storage_path('app/public'),
     ],
 
 ];
