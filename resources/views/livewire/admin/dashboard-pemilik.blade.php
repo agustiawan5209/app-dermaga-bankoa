0567f31b00e5354a041aac786b0b5ef85a2055e9
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-09-03 23:39:28 +0800

    Update

--- resources/views/livewire/admin/dashboard-pemilik.blade.php
+++ resources/views/livewire/admin/dashboard-pemilik.blade.php
@@ -4,7 +4,7 @@
     <div class="flex flex-wrap">
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
             <!--Metric Card-->
-            <div class="bg-gray-200 border border-gray-800 rounded shadow p-2">
+            <div class="bg-gray-100 border border-gray-800 rounded shadow p-2">
                 <div class="flex flex-row items-center">
                     <div class="flex-shrink pr-4">
                         <div class="rounded p-3 bg-green-600"><i
@@ -21,7 +21,7 @@
         </div>
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
             <!--Metric Card-->
-            <div class="bg-gray-200 border border-gray-800 rounded shadow p-2">
+            <div class="bg-gray-100 border border-gray-800 rounded shadow p-2">
                 <div class="flex flex-row items-center">
                     <div class="flex-shrink pr-4">
                         <div class="rounded p-3 bg-pink-600"><i
@@ -38,64 +38,15 @@
         </div>
         <div class="w-full md:w-1/2 xl:w-1/3 p-3">
             <!--Metric Card-->
-            <div class="bg-gray-200 border border-gray-800 rounded shadow p-2">
+            <div class="bg-gray-100 border border-gray-800 rounded shadow p-2">
                 <div class="flex flex-row items-center">
                     <div class="flex-shrink pr-4">
                         <div class="rounded p-3 bg-yellow-600"><i
                                 class="fas fa-user-plus fa-2x fa-fw fa-inverse"></i></div>
                     </div>
                     <div class="flex-1 text-right md:text-center">
-                        <h5 class="font-bold uppercase text-gray-400">New Users</h5>
+                        <h5 class="font-bold uppercase text-gray-400">Jumlah Kapal</h5>
                         <h3 class="font-bold text-3xl text-gray-600">2 <span class="text-yellow-600"><i
-                                    class="fas fa-caret-up"></i></span></h3>
-                    </div>
-                </div>
-            </div>
-            <!--/Metric Card-->
-        </div>
-        <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-            <!--Metric Card-->
-            <div class="bg-gray-200 border border-gray-800 rounded shadow p-2">
-                <div class="flex flex-row items-center">
-                    <div class="flex-shrink pr-4">
-                        <div class="rounded p-3 bg-blue-600"><i
-                                class="fas fa-server fa-2x fa-fw fa-inverse"></i></div>
-                    </div>
-                    <div class="flex-1 text-right md:text-center">
-                        <h5 class="font-bold uppercase text-gray-400">Server Uptime</h5>
-                        <h3 class="font-bold text-3xl text-gray-600">152 days</h3>
-                    </div>
-                </div>
-            </div>
-            <!--/Metric Card-->
-        </div>
-        <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-            <!--Metric Card-->
-            <div class="bg-gray-200 border border-gray-800 rounded shadow p-2">
-                <div class="flex flex-row items-center">
-                    <div class="flex-shrink pr-4">
-                        <div class="rounded p-3 bg-indigo-600"><i
-                                class="fas fa-tasks fa-2x fa-fw fa-inverse"></i></div>
-                    </div>
-                    <div class="flex-1 text-right md:text-center">
-                        <h5 class="font-bold uppercase text-gray-400">To Do List</h5>
-                        <h3 class="font-bold text-3xl text-gray-600">7 tasks</h3>
-                    </div>
-                </div>
-            </div>
-            <!--/Metric Card-->
-        </div>
-        <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-            <!--Metric Card-->
-            <div class="bg-gray-200 border border-gray-800 rounded shadow p-2">
-                <div class="flex flex-row items-center">
-                    <div class="flex-shrink pr-4">
-                        <div class="rounded p-3 bg-red-600"><i
-                                class="fas fa-inbox fa-2x fa-fw fa-inverse"></i></div>
-                    </div>
-                    <div class="flex-1 text-right md:text-center">
-                        <h5 class="font-bold uppercase text-gray-400">Issues</h5>
-                        <h3 class="font-bold text-3xl text-gray-600">3 <span class="text-red-500"><i
                                     class="fas fa-caret-up"></i></span></h3>
                     </div>
                 </div>
