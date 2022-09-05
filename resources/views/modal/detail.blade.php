21e180f083ca7121fd8c8bb6e3e1dd4b22232367
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-09-05 15:59:07 +0800

    Update detail.blade.php

--- resources/views/modal/detail.blade.php
+++ resources/views/modal/detail.blade.php
@@ -29,11 +29,10 @@
                 </div>
                 <div class="flex">
                     <span class="title-font font-medium text-2xl text-gray-900">Harga Tiket : Rp. {{number_format($harga,0,2)}}</span>
-                    <a href="{{route('Customer.Cekout-Page', ['item'=> $itemID])}}">
-                        <button type="button"
+
+                        <button type="button" wire:click='KirimPembayaran({{$itemID}})'
                         class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Pesan
                         Tiket</button>
-                    </a>
                     <button
                         class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
