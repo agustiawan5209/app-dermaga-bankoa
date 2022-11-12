b04b15480127c779e58692eb356045156227e108
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-12 10:16:08 +0800

    Buat Validation view

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class APIController extends Controller
 {
+    public $mesage = [
+        'required'=> 'wajib',
+        'email'=> 'Wajib email :attributes'
+
+    ];
     public function getPelanggan(){
         $user = User::where('role_id', '=', '3')->get();
         return response()->json($user);
